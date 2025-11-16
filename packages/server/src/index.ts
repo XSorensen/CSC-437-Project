1cc125ad5f186e5faf92409f26f9b35199bded32
buddy <145794157+XSorensen@users.noreply.github.com>
2025-11-16 14:53:03 -0800

    Cleanup: removed commented out routes from index.ts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -29,33 +29,6 @@ app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
 });
 
-/*
-app.get("/arcraiders/", (req: Request, res: Response) => {
-  ArcRaiders.index().then((data) => {
-    if(data) res
-      .set("Content-Type", "application/json")
-      .send(JSON.stringify(data));
-
-    else res
-      .status(404).send();
-  });
-});
-
-app.get("/arcraiders/:userid", (req: Request, res: Response) => {
-  const {userid} = req.params;
-
-  ArcRaiders.get(userid).then((data) => {
-
-    console.log(data)
-
-    if (data) res
-      .set("Content-Type", "application/json")
-      .send(JSON.stringify(data));
-    else res
-      .status(404).send();
-  });
-});
-*/
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
